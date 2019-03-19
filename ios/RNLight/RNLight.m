e01e147aa8eb31d09fd58915641ebde3d6bf5293
yuan <yuantian100@gmail.com>
2019-03-19 12:27:58 +1100

    organize folder structure

---
+++ ios/RNLight/RNLight.m
@@ -0,0 +1,57 @@
+#import "AVFoundation/AVFoundation.h"
+#import "RNLight.h"
+#import "Light.h"
+
+@implementation RNLight
+
+RCT_EXPORT_MODULE();
+
+RCT_EXPORT_METHOD(turnLightOn)
+{
+    Light *light = [[Light alloc] init];
+    [light turnLightOn];
+    [self onLightTurnOn];
+}
+
+RCT_EXPORT_METHOD(turnLightOff)
+{
+    Light *light = [[Light alloc] init];
+    [light turnLightOff];
+    [self onLightTurnOff];
+}
+
+RCT_EXPORT_METHOD(toggle)
+{
+    Light *light = [[Light alloc] init];
+    bool isLightActive = [light isLightActive];
+    [light toggle];
+    if (isLightActive) {
+        [self onLightTurnOff];
+    }else{
+        [self onLightTurnOn];
+    }
+}
+
+RCT_EXPORT_METHOD(isLightActive:(RCTResponseSenderBlock)callback)
+{
+    Light *light = [[Light alloc] init];
+    NSArray *isLightActive = [light isLightActive] ? @[@(YES)] : @[@(NO)];
+    callback(isLightActive);
+}
+
+- (NSArray<NSString *> *)supportedEvents
+{
+    return @[@"onLightTurnOn", @"onLightTurnOff"];
+}
+
+- (void)onLightTurnOn
+{
+    [self sendEventWithName:@"onLightTurnOn" body:@""];
+}
+
+- (void)onLightTurnOff
+{
+    [self sendEventWithName:@"onLightTurnOff" body:@""];
+}
+
+@end
